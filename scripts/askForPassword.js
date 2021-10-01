c2dac252d33ec9acc087f21835c4dab298b210e0
Quiquoqua48 <quiquoqua48@gmail.com>
2021-10-01 11:57:17 +0200

    ask for password protected

---
+++ scripts/askForPassword.js
@@ -0,0 +1,9 @@
+    function passwd(){
+        var password = prompt('Inserisci la password:');
+        if(password == "Ab050272"){
+        window.open("https://sviluppofwd.github.io/appMovimentoTrancia/appMovimentoTrancia.apk")    
+        }else{
+        alert("Password errata, riprova");
+        }
+    }
+
